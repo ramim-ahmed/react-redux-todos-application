f491967d8e8087b0382c7ee42517ed46147bb87e
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-09-01 12:26:12 +0600

    some-updates

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navigation() {
   return (
     <div className="fixed top-0 left-0 text-center w-full header bg-indigo-500 py-4 text-white font-bold text-lg shadow-lg">
-      Simple Todo Application with Redux
+      Todo Application with React-Redux
     </div>
   );
 }
